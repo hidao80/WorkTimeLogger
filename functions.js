c5fad9a34b9a574153bd597393931cb3b7ff50b6
hidao <hidao80@users.noreply.github.com>
2018-07-22 14:27:52 +0900

    fix: logArray not writing

--- functions.js
+++ functions.js
@@ -6,6 +6,7 @@ var maxItemNum = 0;
 var logArray;
 const WORK_TIME_LOG = "worktimelog";
 const ITEM_LIST = "itemList";
+const LOG_FILE = "work_time_log.json";
 
 function addItem(_name = undefined) {
   let name;
@@ -38,7 +39,8 @@ function writeLog(text) {
   let now = new Date();
   let data = {};
   data[now.toLocaleTimeString()] = text; 
-  localStorage.setItem(WORK_TIME_LOG, data);
+  logArray.push(data);
+  localStorage.setItem(WORK_TIME_LOG, JSON.stringify(logArray));
 }
 
 function saveLog() {
@@ -47,7 +49,7 @@ function saveLog() {
   //ファイルを作ってダウンロードします。
   let resultJson = JSON.stringify(localStorage.getItem(WORK_TIME_LOG));
   let downLoadLink = document.createElement("a");
-  downLoadLink.download = $scope.fileName;
+  downLoadLink.download = LOG_FILE;
   downLoadLink.href = URL.createObjectURL(new Blob([resultJson], {type: "text.plain"}));
   downLoadLink.dataset.downloadurl = ["text/plain", downLoadLink.download, downLoadLink.href].join(":");
   downLoadLink.click();
@@ -56,6 +58,8 @@ function saveLog() {
 function init() {
   if (localStorage.getItem(ITEM_LIST) !== undefined) {
     itemList = JSON.parse(localStorage.getItem(ITEM_LIST));
+
+    if (itemList === null) itemList = new Array();
     
     for (let i in itemList) { 
       if (itemList[i] != "" ) addItem(itemList[i]);
@@ -66,6 +70,8 @@ function init() {
 
   if (localStorage.getItem(WORK_TIME_LOG) !== undefined) {
     logArray = JSON.parse(localStorage.getItem(WORK_TIME_LOG));    
+
+    if (logArray === null) logArray = new Array();
   } else {
     logArray = new Array();
   }
