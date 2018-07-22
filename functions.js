e4e6dc4bdf8146993a36cc3c2cd21e41b8c58d25
hidao <hidao80@users.noreply.github.com>
2018-07-22 11:53:57 +0900

    feat: Work time log json dowloadable.

--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@ var $ = (id) => {
   return document.querySelector(id);
 }
 
-function add() {
+function addItem() {
   let name = $("#input_box").value;
   $("#items").insertAdjacentHTML('beforeend','<div class=\'item\' onclick=\'writeLog("'+name+'")\'>'+name+'</div>');
 }
@@ -10,4 +10,13 @@ function add() {
 function writeLog(text) {
   let now = new Date();
   localStorage.setItem(now.toLocaleTimeString(), text);
+}
+
+function saveLog() {
+//  Object.keys(localStorage).forEach(function(key) { 
+//    console.log(key);
+//  });
+
+  var href = "data:application/octet-stream," + encodeURIComponent(JSON.stringify(localStorage));
+  location.href = href;
 }
